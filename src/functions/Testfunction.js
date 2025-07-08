aab71133aef4fb2d80d0fddf9f36d48fa126ed4d
William <gilliamwlover@gmail.com>
2025-07-08 09:00:05 -0700

    Added new blob storage lcoation

--- src/functions/Testfunction.js
+++ src/functions/Testfunction.js
@@ -1,4 +1,4 @@
-const { BlobServiceClient } = require("@azure/amfgblobstorage");
+const { BlobServiceClient } = require("@azure/storage-blob");
 
 
 const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
