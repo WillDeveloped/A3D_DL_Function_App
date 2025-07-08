451198642138eee4c31f93f7154d0ccfa6496c43
William <gilliamwlover@gmail.com>
2025-07-08 09:23:49 -0700

    Adjusted route

--- src/functions/Datalake.js
+++ src/functions/Datalake.js
@@ -3,7 +3,7 @@ const { app } = require('@azure/functions');
 app.http('Datalake', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    route:'Testfunction.js',
+    route:'functions/Testfunction.js',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
